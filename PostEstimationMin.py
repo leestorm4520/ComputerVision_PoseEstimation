232661c3d75bdb08ac8adb6df2097188aad0a7dd
DuyLe <minhduy4520@gmail.com>
2022-07-11 21:43:28 -0500

    update

--- PostEstimationMin.py
+++ PostEstimationMin.py
@@ -1,9 +1,27 @@
 import cv2
 import mediapipe as mp
+import time
 
-cap=cv2.VideoCapture(0)
+mpPose=mp.solutions.pose
+pose=mpPose.Pose()
+cap=cv2.VideoCapture("examples_media_video.avi")
+pTime=0
 
 while True: 
     success, img= cap.read()
+    imgRGB=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    result=pose.process(imgRGB)
+    print(result)
+
+    cTime=time.time()
+    fps=1/(cTime-pTime)
+    pTime=cTime
+    cv2.putText(img, str(int(fps)), (70,50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+
+    cap.release()
+    cv2.destroyAllWindows()
